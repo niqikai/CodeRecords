5d4f2cad3b5bfcb52562aed0817152c9b41efcb0
niqikai <niqikai@aliyun.com>
2020-01-30 13:31:44 +0800

    eureka

--- eureka-api/src/main/java/com/example/eurekaapi/EurekaApiApplication.java
+++ eureka-api/src/main/java/com/example/eurekaapi/EurekaApiApplication.java
@@ -3,10 +3,18 @@ package com.example.eurekaapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class EurekaApiApplication {
+
+	@RequestMapping("/hello")
+	public String hello(String name) {
+		return "hello-eureka: " + name;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApiApplication.class, args);
