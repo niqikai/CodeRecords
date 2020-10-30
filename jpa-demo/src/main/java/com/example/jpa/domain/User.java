7d9914b921f5656e834a9ee29f0de21708f70ac5
niqikai <niqikai@aliyun.com>
2020-10-30 22:39:24 +0800

    init

---
+++ jpa-demo/src/main/java/com/example/jpa/domain/User.java
@@ -0,0 +1,17 @@
+package com.example.jpa.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class User {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
---
+++ springboot/src/test/java/com/example/springboot/MainTest.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Map<String, Long> map = new HashMap<>();
+
+        map.put("hello", 10L);
+
+        
+    }
+
+}
